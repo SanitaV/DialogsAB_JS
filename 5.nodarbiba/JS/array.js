b6a2eabfb03ca6de7f0c2da35cea6bc0e92abaef
SanitaV <ibsak@inbox.lv>
2023-04-27 20:57:19 +0300

    20230427_20_57_3

---
+++ 5.nodarbiba/JS/array.js
@@ -0,0 +1,35 @@
+var x1 = 1,
+x2 = 2,
+x3 = 3;
+var delta_x1 = x2 - x1;
+var delta_x2 = x3 - x2;
+
+var x_coord = [11, 22, 306];
+console.log(x_coord[0]);
+console.log(x_coord[1]);
+console.log(x_coord[2]);
+
+var coord = [
+[11, 22, 306],
+[89, 96, 45],
+];
+console.log(coord[0]);
+console.log(coord[0][0]);
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
+console.log(fruits[2]);
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits1.join(" * ");
+console.log(fruits1[0]);
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+fruits2.pop();
+document.getElementById("demo2").innerHTML = fruits2;
+//skat.kļūdu
+
+
