46d622ccaacf30e7f141c62bb01c6091fff8a1c8
SanitaV <ibsak@inbox.lv>
2023-04-29 21:03:18 +0300

    20230429_21_03_1

---
+++ 6.nodarbiba/js/if.js
@@ -0,0 +1,9 @@
+if (new Date().getHours() < 18) {
+    document.getElementById("demo_if").innerHTML = "Good day!";
+    document.getElementById("demo_if").innerHTML =
+      document.getElementById("demo_if").innerHTML + " Good day one more time!";
+  }
+  
+  if (new Date().getHours() < 18) console.log("Good day!");
+  
+  console.log("Good day one more time!");
